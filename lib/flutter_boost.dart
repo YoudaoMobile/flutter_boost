2b854758d074352c8da57a5f5022f7aa311fd5ba
车德超 <chedch@rd.netease.com>
2020-06-29 16:18:39 +0800

    FIX::pageinfo不是map，并且多调一次Appear的问题

--- lib/flutter_boost.dart
+++ lib/flutter_boost.dart
@@ -73,12 +73,12 @@ class FlutterBoost {
       singleton.channel.invokeMethod<Map>('pageOnStart').then((Map pageInfo) {
         if (pageInfo == null || pageInfo.isEmpty) return;
 
-        if (pageInfo.containsKey("name") &&
-            pageInfo.containsKey("params") &&
-            pageInfo.containsKey("uniqueId")) {
-          ContainerCoordinator.singleton.nativeContainerDidShow(
-              pageInfo["name"], pageInfo["params"], pageInfo["uniqueId"]);
-        }
+//        if (pageInfo.containsKey("name") &&
+//            pageInfo.containsKey("params") &&
+//            pageInfo.containsKey("uniqueId")) {
+//          ContainerCoordinator.singleton.nativeContainerDidShow(
+//              pageInfo["name"], pageInfo["params"], pageInfo["uniqueId"]);
+//        }
       });
     });
   }
